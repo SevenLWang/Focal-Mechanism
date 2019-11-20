bc37961986ba796237dd2d24e05f6545bfc4e180
SevenLWang <sevenlwang@gmail.com>
2019-11-20 10:12:34 +0800

    Add the P-wave arrival into the weight.dat generation

--- Generate_Weight.sh
+++ Generate_Weight.sh
@@ -2,6 +2,6 @@
 dire=`pwd`
 newdire=$dire/rot_dire
 cd $newdire
-saclst dist az f *.z   | awk '{gsub(".z","",$1); printf "%s %d %d %d %d %d %d %f %f\n", $1,$2,1,1,1,1,1,0,0}' | sort -n --key=3 > weight.dat
+saclst dist az t1 f *.z   | awk '{gsub(".z","",$1); printf "%s %d %d %d %d %d %d %f %f\n", $1,$2,1,1,1,1,1,$3,0}' | sort -n --key=3 > weight.dat
 #saclst gcarc az f *.z   | awk '{gsub(".z","",$1); printf "%s %f %3.1f %s\n", $1,$2,$2,"1 1 1 1 1 0 0 0"}' | sort -n --key=3 > weight.dat
 cd $nowdir
